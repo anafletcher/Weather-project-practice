7a822e09f4515b015774691514629ff0c0a7bd1b
anafletcher <153273057+anafletcher@users.noreply.github.com>
2023-12-28 16:36:45 +0000

    Added basic CSS before adding Axios and city search component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function submitCity(event) {
+  event.preventDefault();
+  let searchFormInput = document.querySelector("#search-form-input");
+  let cityHeaderInput = document.querySelector("#city-header");
+  cityHeaderInput.innerHTML = searchFormInput.value;
+}
 
+let searchFormElement = document.querySelector("#seach-form");
+searchFormElement = addEventListener("submit", submitCity);
