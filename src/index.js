1582e24e689cabe12cc6a992497f7668b818cf2e
anafletcher <153273057+anafletcher@users.noreply.github.com>
2024-01-02 18:51:50 +0000

    Added time and weather update functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,59 @@
+function refreshWeather(response) {
+  let tempertureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  tempertureElement.innerHTML = Math.round(temperature);
+  let cityHeaderInput = document.querySelector("#city-header");
+  cityHeaderInput.innerHTML = response.data.city;
+  let condittionsElement = document.querySelector("#conditions");
+  condittionsElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatDate();
+
+  let date = new Date(response.data.temperature.time * 1000);
+
+  console.log(response);
+}
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tueday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = days[date.getDay()];
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function submitCity(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
-  let cityHeaderInput = document.querySelector("#city-header");
-  cityHeaderInput.innerHTML = searchFormInput.value;
+  searchCity(searchFormInput.value);
+}
+function searchCity(city) {
+  let apiKey = "f3c5613898a1043cbte4a77d8c1bcfo0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 let searchFormElement = document.querySelector("#seach-form");
 searchFormElement = addEventListener("submit", submitCity);
+
+searchCity("London");
